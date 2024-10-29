7b6b34965c0b208f4f16deb452e35315c994d876
huiqinghu <57228170+huiqinghu@users.noreply.github.com>
2024-10-29 16:59:59 +0800

    Add files via upload

---
+++ calculate_mean_FC.m
@@ -0,0 +1,9 @@
+function [mean_pos, mean_neg, mean_abs] = calculate_mean_FC(mat_data)
+
+mat_data_pos = mat_data(~isnan(mat_data) & mat_data > 0);
+mat_data_neg = mat_data(~isnan(mat_data) & mat_data < 0);
+mat_data_abs = abs(mat_data); mat_data_abs = mat_data_abs(~isnan(mat_data_abs));
+mean_pos = mean(mat_data_pos);
+mean_neg = mean(mat_data_neg);
+mean_abs = mean(mat_data_abs);
+end
